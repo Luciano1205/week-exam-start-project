4678fcc6484ddfc72cc44c3e3b12dc99f661d4da
Luciano Barbosa <julsuatgol.105@gmail.com>
2026-09-03 14:26:35 -0500

    Marzo

---
+++ src/main/java/com/example/model/Sightings.java
@@ -0,0 +1,28 @@
+package com.example.model;
+
+public class Sightings {
+    private Integer id;
+    private String sightingCode;
+    private String name;
+    private String description;
+    private String scientificName;
+    private String sightedAt;
+    private String location;
+    private Integer quantity;
+    private Integer confidenceLevel;
+    private Integer expeditionId;
+
+
+    public Sightings(Integer id;
+     String sightingCode;
+     String name;
+     String description;
+     String scientificName;
+     String sightedAt;
+     String location;
+     Integer quantity;
+     Integer confidenceLevel;
+     Integer expeditionId;){
+
+    }
+}
